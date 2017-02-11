53555ed8f0af0367fa1e64755e8fea0c75d9abf5
mihirk <mihir.khatwani@gmail.com>
2017-02-12 03:41:48 +0530

    Fixing controller formatting, and adding gzip compression

--- src/main/java/org/nhsrc/web/RegionDataController.java
+++ src/main/java/org/nhsrc/web/RegionDataController.java
@@ -3,9 +3,10 @@ package org.nhsrc.web;
 import org.nhsrc.domain.State;
 import org.nhsrc.repository.StateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,10 +23,12 @@ public class RegionDataController {
     private StateRepository stateRepository;
 
     @RequestMapping(value = "/states", method = RequestMethod.GET)
-    public Set<State> getModifiedEntities(@RequestParam("lastSyncedDate")
-                                          @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm:ss") Date lastSyncedDate) {
+    public ResponseEntity<Set<State>> getModifiedEntities(
+            @RequestParam("lastSyncedDate")
+            @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm:ss") Date lastSyncedDate) {
         PageRequest page = new PageRequest(0, 10);
-        Page<State> states = stateRepository.findByLastModifiedDateGreaterThanOrderById(lastSyncedDate, page);
-        return new HashSet<>(states.getContent());
+        HashSet<State> states = new HashSet<>(
+                stateRepository.findByLastModifiedDateGreaterThanOrderById(lastSyncedDate, page).getContent());
+        return new ResponseEntity<Set<State>>(states, HttpStatus.OK);
     }
 }
--- src/test/java/org/nhsrc/web/RegionDataControllerTest.java
+++ src/test/java/org/nhsrc/web/RegionDataControllerTest.java
@@ -2,16 +2,17 @@ package org.nhsrc.web;
 
 import org.junit.Test;
 import org.nhsrc.common.AbstractWebIntegrationTest;
-import org.nhsrc.domain.State;
 
+import java.util.LinkedHashMap;
 import java.util.Set;
 
 public class RegionDataControllerTest extends AbstractWebIntegrationTest {
     @Test
     public void testStuff() throws Exception {
-        Set<State> states = (Set<State>) getResponse("states", Set.class);
-        for (State state : states) {
-            System.out.println(state.getName());
+        Set states = (Set) this.getResponse("states?lastSyncedDate=12-02-1990 01:01:47", Set.class);
+        for (Object state : states) {
+            LinkedHashMap state1 = (LinkedHashMap) state;
+            System.out.println(state1.get("name"));
         }
     }
 }
