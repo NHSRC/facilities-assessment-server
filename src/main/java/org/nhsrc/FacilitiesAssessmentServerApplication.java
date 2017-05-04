303e3416d155820666864dc86fc7bf3603a609ce
mihirk <mihir.khatwani@gmail.com>
2017-05-04 17:13:45 +0530

    Removing unnecessary interceptor

--- src/main/java/org/nhsrc/FacilitiesAssessmentServerApplication.java
+++ src/main/java/org/nhsrc/FacilitiesAssessmentServerApplication.java
@@ -1,13 +1,11 @@
 package org.nhsrc;
 
 import org.nhsrc.config.DatabaseConfiguration;
-import org.nhsrc.config.LengthInterceptor;
 import org.nhsrc.config.RestConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.TimeZone;
@@ -16,11 +14,6 @@ import java.util.TimeZone;
 @Import({RestConfiguration.class, DatabaseConfiguration.class})
 @EnableJpaAuditing
 public class FacilitiesAssessmentServerApplication extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LengthInterceptor()).addPathPatterns("/**");
-    }
 
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Calcutta"));
