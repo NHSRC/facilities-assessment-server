a69345d5ab5b94554bcaaffcb3bab883ada538d8
Vivek Singh <petmongrels@gmail.com>
2017-09-14 16:58:30 +0530

    handle duplicate constraints caused by direct change to production views created to support drill down in metabase

--- src/main/resources/db/migration/V34__ModifyUniqueConstraints.sql
+++ src/main/resources/db/migration/V34__ModifyUniqueConstraints.sql
@@ -1,2 +1,3 @@
 ALTER TABLE facility_assessment DROP CONSTRAINT IF EXISTS facility_assessment_series_name_facility_id_key;
+ALTER TABLE facility_assessment DROP CONSTRAINT IF EXISTS facility_assessment_series_name_facility_id_assessment_tool_key;
 ALTER TABLE facility_assessment ADD UNIQUE (series_name, facility_id, assessment_tool_id);
