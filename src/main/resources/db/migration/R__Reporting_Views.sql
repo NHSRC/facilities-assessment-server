8486b79fa84fbfd79d3e14ebc2bb459800e3f7b4
Vivek Singh <petmongrels@gmail.com>
2017-09-20 14:14:44 +0530

    include state and facility type in the checkpoint score view

--- src/main/resources/db/migration/R__Reporting_Views.sql
+++ src/main/resources/db/migration/R__Reporting_Views.sql
@@ -23,7 +23,9 @@ CREATE OR REPLACE VIEW checkpoint_scores_aoc AS
     format('%s (%s)', aoc.reference, aoc.name) AS area_of_concern,
     department.id                              AS department,
     c.name                                     AS checkpoint,
-    cs.score                                   AS score
+    cs.score                                   AS score,
+    state.id                                   AS state,
+    facility_type.id                           AS facility_type
   FROM checkpoint_score cs
     INNER JOIN checkpoint c ON cs.checkpoint_id = c.id
     LEFT OUTER JOIN checklist cl ON cl.id = cs.checklist_id
@@ -33,6 +35,9 @@ CREATE OR REPLACE VIEW checkpoint_scores_aoc AS
     LEFT OUTER JOIN area_of_concern aoc ON aoc.id = s.area_of_concern_id
     LEFT OUTER JOIN facility_assessment fa ON cs.facility_assessment_id = fa.id
     LEFT OUTER JOIN facility ON fa.facility_id = facility.id
+    LEFT OUTER JOIN facility_type ON facility.facility_type_id = facility_type.id
+    LEFT OUTER JOIN district ON facility.district_id = district.id
+    LEFT OUTER JOIN state ON district.state_id = state.id
     LEFT OUTER JOIN assessment_tool ON fa.assessment_tool_id = assessment_tool.id
     LEFT OUTER JOIN assessment_tool_mode ON assessment_tool_mode.id = assessment_tool.assessment_tool_mode_id;
 
@@ -57,12 +62,12 @@ CREATE OR REPLACE VIEW checkpoint_denormalised AS
 
 CREATE OR REPLACE VIEW checkpoint_score_denormalised AS
   SELECT
-    checkpoint_score.id    checkpoint_score_id,
-    facility_assessment.id facility_assessment_id,
-    measurable_element.id  measurable_element_id,
-    standard.id            standard_id,
-    area_of_concern.id     area_of_concern_id,
-    checklist.id           checklist_id,
+    checkpoint_score.id     checkpoint_score_id,
+    facility_assessment.id  facility_assessment_id,
+    measurable_element.id   measurable_element_id,
+    standard.id             standard_id,
+    area_of_concern.id      area_of_concern_id,
+    checklist.id            checklist_id,
     assessment_tool.id      assessment_tool_id,
     assessment_tool_mode.id assessment_tool_mode_id,
     state.id                state_id
