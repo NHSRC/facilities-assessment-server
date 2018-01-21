24171361de761b9f9456360fbd5b16636a56d254
Vivek Singh <petmongrels@gmail.com>
2018-01-21 23:47:18 +0530

    added support for https and http both

--- src/main/java/org/nhsrc/config/SecurityConfiguration.java
+++ src/main/java/org/nhsrc/config/SecurityConfiguration.java
@@ -1,7 +1,5 @@
 package org.nhsrc.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +14,7 @@ import org.springframework.security.config.annotation.web.configurers.Expression
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import javax.sql.DataSource;
 import java.util.Arrays;
 
 @Configuration
