10a1b13fe31d5c7eafab2f8df99bd665bcc2257d
Vivek Singh <petmongrels@gmail.com>
2019-04-09 12:30:47 +0530

    one controller to handle all non functonal requests

---
+++ src/main/java/org/nhsrc/web/framework/InfraController.java
@@ -0,0 +1,26 @@
+package org.nhsrc.web.framework;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class InfraController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public void root(HttpServletResponse httpServletResponse) {
+        httpServletResponse.setHeader("Location", "/app/index.html");
+        httpServletResponse.setStatus(302);
+    }
+
+    @RequestMapping(value = "/api/ping", method = RequestMethod.GET)
+    public String ping() {
+        return "pong";
+    }
+
+    @RequestMapping(value = "/api/error/throw", method = {RequestMethod.GET})
+    public void throwError() {
+        throw new RuntimeException("Throwing error to check whether Bugsnag records it");
+    }
+}
